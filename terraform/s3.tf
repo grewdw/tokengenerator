804e1cac9cbf0af9598299a132bb55361d4ebdfc
David G <davidgrew@hotmail.co.uk>
2021-03-03 19:53:27 +0000

    format terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,12 +7,12 @@ resource "aws_s3_bucket" "tabwriter_s3_bucket" {
 }
 
 resource "aws_s3_bucket_object" "cloudwatch_config" {
-  bucket  = aws_s3_bucket.tabwriter_s3_bucket.id
-  key     = "cloudwatch_log_config.txt"
+  bucket = aws_s3_bucket.tabwriter_s3_bucket.id
+  key    = "cloudwatch_log_config.txt"
   content = templatefile("${path.root}/templates/cloudwatch_log_config.json.tpl", {
     LOG_GROUP_NAME     = aws_cloudwatch_log_group.tabwriter_log_group.name
     ACCESS_STREAM_NAME = aws_cloudwatch_log_stream.nginx_access_log_stream.name
     ERROR_STREAM_NAME  = aws_cloudwatch_log_stream.nginx_error_log_stream.name
   })
-  etag 		= filemd5("${path.root}/templates/cloudwatch_log_config.json.tpl")
+  etag = filemd5("${path.root}/templates/cloudwatch_log_config.json.tpl")
 }
